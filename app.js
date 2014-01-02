e58bf6130e064d0790d32792b565892a8c265743
Jim Wharton <jamesawharton@gmail.com>
2014-01-01 23:07:00 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var pg = require('pg');
+var routes = require('./routes');
+
+var http = require('http');
+var path = require('path');
+
+var conString = "postgres://utilisync:utilisync@localhost/utilisync";
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+});
+
+var app = express();
+
+//cheap quick middleware:
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(allowCrossDomain);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var sketch = require('./routes/sketch');
+sketch.inject(client);
+
+app.post('/sketch/save', sketch.save);
+app.get('/sketch/:id', sketch.find);
+app.get('/sketch/download/:id', sketch.download);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/sketch.js
@@ -0,0 +1,90 @@
+var CLIENT;
+var Terraformer = require('terraformer');
+var ArcGIS = require('terraformer-arcgis-parser');
+var fs = require('fs');
+var geojson2shape = require('geojson2shape');
+var path = require('path');
+
+exports.inject = function(con) {
+  CLIENT=con;
+};
+
+exports.save = function(req, res) {
+  var sketches = req.body;
+  var requestId;
+  var parsed;
+  var query;
+  //insert request params into db
+  sketches.forEach(function(sketch) {
+    parsed = ArcGIS.parse(sketch.geometry, {sr:sketch.geometry.spatialReference.wkid});
+    var geomText = JSON.stringify(parsed);
+    var query = CLIENT.query(['INSERT INTO sketches',
+                              '(username, sketch_json, geom) ',
+                              'VALUES($1, $2, ST_SetSRID(ST_GeomFromGeoJSON($3), 102100)) ',
+                              'RETURNING id'].join('\n'), ['jim',
+                                                           sketch,
+                                                           geomText
+                                                          ]);
+
+    query.on('error', function(error) {
+      console.log('Error processing SQL: ' + error);
+    });
+    query.on('end', function () {
+      res.json({status:"success"});
+    });
+  })
+
+};
+
+exports.download = function(req, res) {
+  var id = req.params.id;
+  var query = CLIENT.query('SELECT username, ST_AsGeoJSON(geom) AS geometry FROM sketches WHERE id = $1', [id]);
+  var request;
+  query.on('row', function(row) {
+    request = row;
+  });
+  query.on('end', function(result) {
+    var output = request.geometry.toString();
+    var downloadPath = path.resolve(__dirname+'/../public');
+    var infile = request.username + new Date().getTime() + '.json';
+    fs.writeFile(downloadPath+'/'+infile, output, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("GeoJSON file saved!");
+
+        var zip = true;
+        var outfile = infile.slice(0, -5) + '.shp';
+        var download = outfile.slice(0, -4) + '.zip';
+        geojson2shape(downloadPath+'/'+infile, downloadPath+'/'+outfile, zip, function(err){
+          if(err) {
+            throw err;
+          } else {
+            res.setHeader('Content-disposition', 'attachment; filename=' + download);
+            res.setHeader('Content-type', 'application/x-gzip');
+            // res.sendfile(downloadPath + '/' + outfile.slice(0, -4) + '.zip');
+            var filestream = fs.createReadStream(downloadPath + '/' + download);
+            filestream.pipe(res);
+          }
+        });
+      }
+    });
+
+  });
+
+
+
+
+}
+
+exports.find = function(req, res) {
+  var id = req.params.id;
+  var query = CLIENT.query('SELECT * FROM sketches WHERE id = $1', [id]);
+  var request;
+  query.on('row', function(row) {
+    request = row;
+  });
+  query.on('end', function(result) {
+    res.json(request);
+  });
+}
